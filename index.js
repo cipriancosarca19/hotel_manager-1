d18d817fe1228c85f83c3eeac0c1deaedfd27a13
Joe Kitaj <joekitaj@gmail.com>
2017-02-14 11:27:49 -0500

    Added body-parser package requirements

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 // Set up Node to use EJS as the templating engine
 app.set('view engine', 'ejs');
 
+// Sets up body-parser to only return relavent data on POST requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // Set up Express to use 'public' as the static assests folder
 const path = require('path');
 
