f84f7fb011c963a104d75882b7164e16e682cdc0
Joe Kitaj <joekitaj@gmail.com>
2017-02-14 11:56:14 -0500

    Changed object syntax

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 
 // Sets up body-parser to only return relavent data on POST requests
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Set up Express to use 'public' as the static assests folder
