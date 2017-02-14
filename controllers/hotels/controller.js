853ac9c54beebe124aae7b1aca8983a02f9f108d
Joe Kitaj <joekitaj@gmail.com>
2017-02-14 14:18:06 -0500

    Templating works so that submitted hotels render

--- controllers/hotels/controller.js
+++ controllers/hotels/controller.js
@@ -3,37 +3,43 @@ let controller = {};
 
 controller.index = (req, res) => {
   // Automatically looks for views/foldername/filename.ejs
-  res.render('hotels/index')
+  Hotel
+  .findAll()
+  .then((hotel_data) => {
+    res.render('hotels/index', {
+      hotels: hotel_data
+    })
+  });
 };
 
 controller.new = (req, res) => {
   res.render('hotels/new')
-}
+};
 
 controller.show = (req, res) => {
-}
+};
 
 controller.create = (req, res) => {
   // Grabs the json from the form submission, goes over to the model, and appends the data to the DB
-  Hotel.save(req.body.hotel)
+  Hotel
+  .save(req.body.hotel)
   .then(() => {
     res.redirect('/hotels');
   })
+  // Redirects us back to /hotels
   .catch((err) => {
     res
     .status(400)
     .send(err);
   });
-  // Redirects us back to /hotels
-
-}
+};
 
 controller.update = (req, res) => {
 
-}
+};
 
 controller.destroy = (req, res) => {
 
-}
+};
 
 module.exports = controller;
