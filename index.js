a5cb3ba0ce70aa471c5b7ed6c04e41a328488a4e
Joe Kitaj <joekitaj@gmail.com>
2017-02-14 15:35:03 -0500

    Set up method-override middleware

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Set up Express to use 'public' as the static assests folder
+
 const path = require('path');
 
+// Set up method overrride to allow HTML forms to perform PUT and DELETE requests
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+// Set up Express to use 'public' as the static assests folder
 app.use(express.static(
   path.join(__dirname, 'public')
 ));
